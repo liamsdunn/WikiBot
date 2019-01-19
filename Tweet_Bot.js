018166c76063cb149b839fdc4bbe13f55f7fd48b
liamsdunn <liamsdunn1@gmail.com>
2019-01-18 19:59:32 -0500

    Style and comments

--- Tweet_Bot.js
+++ Tweet_Bot.js
@@ -1,12 +1,12 @@
 
 
 // Our Dependancies 
-var Twit = require('twit')
-var text = require('./Wikiscraper.js')
+var Twit = require('twit');
+var text = require('./Wikiscraper.js');
 var fs = require('fs');
 
 // Importing the personal keys to access twitter API
-var keys = fs.readFileSync('./keys.txt', 'utf8').split('\r\n')
+var keys = fs.readFileSync('./keys.txt', 'utf8').split('\r\n');
 
 // Initilizing our twit object with our imported keys 
 var T = new Twit({
@@ -14,20 +14,20 @@ var T = new Twit({
   consumer_secret:      keys[1],
   access_token:         keys[2],
   access_token_secret:  keys[3],
- // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
- // strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+ timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+ strictSSL:            true,     // optional - requires SSL certificates to be valid.
+});
 
 // Running the tweet function
  
-console.log('Bot is starting')
-tweet()
-console.log('Tweet Successful')
+console.log('Bot is starting');
+tweet();
+console.log('Tweet Successful');
 
 // The tweet function calls Wikiscraper to get a sentence to tweet
 async function tweet() {
- 	var contents = String(await text.foo());
- 	console.log(contents)
+ 	var contents = String(await text.getTweetText());
+ 	console.log(contents);
 	T.post('statuses/update', { status: contents }, function(err, data, response) {console.log(data)})
 }
 
--- Wikiscraper.js
+++ Wikiscraper.js
@@ -6,61 +6,80 @@ var website = 'https://en.wikipedia.org/w/index.php?title=';
 var parse = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 var random = 'https://en.wikipedia.org/wiki/Special:Random';
 var rege = /en.wikipedia.org\/w\/index.php\?title=[\w\d()\s.,?]+/gi;
-var extract = /"extract":"[\W\w\d()\s.,;?-]+,"extract_html"/;
+var extract = /"extract":"[\W\w\d()\s.,;:?-]+,"extract_html"/;
 
 /* 
   @desc - Scrapes text from the input url using the input regular expression. **does MORE
   @param - String url, regularExpression regex
   @return - A string array of matches to our regex 
 */
+
 function scrape(url, regex) {
   var quote;
 
   return new Promise(function(resolve, reject) {
     request(url, function(error, response, body) {
-      quote = body.match(regex);
-      resolve(quote);
+
+      if(response.statusCode == 200){
+        quote = body.match(regex);
+        resolve(quote); 
+
+      } else {
+        console.log(url);
+        console.log(body.match(regex));
+        console.log('error: '+ response.statusCode);
+      }
+      
     });
   });
 }
 
-/* 
-  @desc - 
-  @param - String url, regularExpression regex
-  @return - A string array of matches to our regex 
-*/
-module.exports = {
-  foo: async function getText() {
-  
-    var urls = []
-    var quote = await scrape(random, rege);
 
+
+module.exports = {
+  /* 
+    @desc - This function uses the url to call the random wikipedia api to gain access to a random article. 
+            Then it parses the entire page looking for other wikipedia links. Then once all junk urls are removed,
+            the function calls the first of the new urls, to then grab information from that article. It then takes
+            the plain text, sanitizes and splices the sentences, then chooses a sentence at random to return.
+    @param - String url, RegExp regex
+    @return - A string of a random sentence from a random wikipedia article. 
+  */
+
+  getTweetText: async function getText() {
+    
+    // Calling https://en.wikipedia.org/wiki/Special:Random to direct to a random url, 
+    // then links are parsed by scrape
+    
+    var quote = await scrape(random, rege);
+    var urls = [];
+
+    // Loops through removing all junk urls
     for (i = 0; i < quote.length; i++) {
   	 if (!quote[i].match(/(Template|Special)/)) {
-  		urls.push(quote[i])
+  		urls.push(quote[i]);
   	 }
     }
 
-    var article_url = urls[1].split('=')[1]
-    var result = await scrape(parse.concat(article_url), extract)
-    var text = result[0].replace(new RegExp('(","extract_html"|"extract":")', 'g'), "")
-    var sentence = text.split("\.")
-    sentence.pop()
+    // Splitting the first url, gaining its title. Then redirecting to the new page to then scrape and sanitize its input. 
+    var article_url = urls[1].split('=')[1];
+    var result = await scrape(parse.concat(article_url), extract);
+    var text = result[0].replace(new RegExp('(","extract_html"|"extract":"|\n\n)', 'g'), "");
+    var sentence = text.split("\.");
+    sentence.pop();
 
-    console.log(text)
-    console.log(sentence)
-    console.log('done')
 
-    var choice
+    // Looping continually chosing random numbers until chosen sentence is less then tweet size limit
+    var choice;
 
     do {
   	 choice = Math.floor(Math.random() * (sentence.length ))
-  	 console.log(choice)
-    } while (sentence[choice].length < 1 && sentence[choice].length + 2 + article_url.length + website.length > 280)
+  	 var tweetLength = sentence[choice].length + 2 + article_url.length + website.length;
+    } while (sentence[choice].length < 1 && tweetLength > 280)
 
-    var Tweet = sentence[choice].concat('. ', website, article_url)
-    console.log(Tweet)
+    // Url from which the sentence was found appended to tweet
+    var Tweet = sentence[choice].concat('. ', website, article_url);
 
-    return Tweet
+    return Tweet;
   }
 };
